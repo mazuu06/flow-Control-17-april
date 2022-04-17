1640ee5810bc36b2a058713d5abaf52ca2349c23
RaNa MazHar Naseer <103133005+Hawk-06@users.noreply.github.com>
2022-04-17 21:02:34 +0500

    Add files via upload

---
+++ Ctrl_Flw_17_april.py
@@ -0,0 +1,64 @@
+############################################### *Sequential* ###############################################
+
+# x = 5
+# y = 7
+# z = x + 9
+# print("value of z is : ", z)
+# print("value of x is : ", x)
+
+############################################### *Selection* ###############################################
+# *** if ***                        *comment
+
+# if 5 > 2:
+#     print("Hello")
+
+# *** if else ***                   *comment
+
+# a = "python"
+
+# if a == "php":
+#     print("php")
+# else:
+#     print("python")
+
+# *** if elif else ***              *comment
+
+# x = 5
+# y = 8
+
+# if x < 5:
+#     print("x is less then 5")
+# elif x > 5:
+#     print("x is greater then 5")
+# else:
+#     print("x is equal to 5")
+
+# *** nested if else ***            *comment
+
+# num1 = 8
+# num2 = 4
+# num3 = 10
+
+# if num1 > num2:
+#     if num1 > num3:
+#         print("num1 is greatest number")
+#     else:
+#         print("other is greater")
+# else:
+#     print("num1 is not greater then num2")
+
+
+############################################### *Repetition* ###############################################
+# *** while loop ***            *comment
+
+# str = "I love Pakistan"
+# x = 1
+# while (x <= 5):
+#     print(str)
+#     x += 1
+
+# *** for loop ***               *comment
+
+lst = ("Hello", "I", "am", "learning", "python")
+for in in range(len(lst)):
+    print(lst[i])
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# flow-Control-17-april
+>>>>> What is Flow Control ?
+The sequence of execution of a any program called flow control.
+There are three types of flow control.
+	1) Sequential
+	2) Selection
+	3) Repetition
+	
+>>>>> What is Sequential Flow Control ?
+A type of program flow control in which all statements executed one by one in a sequence. We can't skip one or more statement. 
+For Example: 
+	print("Hello")
+	print("I'm")
+	print("learning")
+	print("python")
+These all statemets execute one by one.
+
+>>>>> What is Selection Flow Control ?
+A type of flow control in which a statement or set of statement executed from number of code's blocks based on a condition. Selection also called conditional statements.
+There are four type of conditional statements:
+	1) if
+	2) if else
+	3) if elif else
+	4) nested if else
+	
+>>>>> What is Repetition Flow Control ?
+A type of program flow in which a statement or number of statements repeat one or more times with repect to a special condition.Repetition is also known as loops.
+There are two types of loops in python.
+	1) While loop
+	2) For loop
+We use while loop when we does't know the number of iterations.
+We use for loop when we know the number of iterations.
